3776f7df1b4ae7462fcea98d8001f4966fbddb1a
Marc Benedict C. Escatron <marc.escatron07@gmail.com>
2021-01-08 11:47:07 +0800

    finished Section 2: The Basics

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html',
+    styles: [`
+        .online {
+            color: white;
+        }
+    `]
 })
 
-export class ServerComponent {
+export class ServerComponent implements OnInit {
+    allowNewServer: boolean = false;
+    serverCreationStatus: string = "Server is offline";
+    serverName: string = '';
+    isServerCreated: boolean = false;
+    serverList: string[] = [];
 
+    constructor(){
+        setTimeout(() => {
+            this.allowNewServer = true;
+        }, 2000)
+    }
+
+    ngOnInit() {
+
+    }
+
+    onCreateServer = () => {
+        this.isServerCreated = true;
+        this.serverList.push(this.serverName);
+        return this.serverCreationStatus = 'Server is now online!';
+    }
+
+    onUpdateServerName = (e: any) => {
+        console.log('onUpdateServerName events', e.target.value)
+        return this.serverName = e.target.value;
+    }
 }
